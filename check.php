3be8567519ac2c93fcd608b1dc1ed1706aaba9b6
Natthaphong Chaowacharoenphong <6110520134@student.tu.ac.th>
2021-06-11 14:58:37 +0700

    Update

--- check.php
+++ check.php
@@ -1,39 +1,40 @@
-<?php 
-    session_start();
-    include('server.php');
-    $errors = array();
-        $count =  $_POST['count'];
-        $product = $_POST['product'];
+<?php
+session_start();
+include('server.php');
+$errors = array();
 
-        if (empty($count)) {
-            array_push($errors, "count is required");
-        }
+$count =  $_POST['count'];
+$product = $_POST['product'];
 
-        if (empty($product)) {
-            array_push($errors, "product is required");
-        }
-        if (count($errors) == 0) {
-            $query = "SELECT * FROM product WHERE name = '$product'";
-            $result = mysqli_query($conn, $query);
-            if (!empty($result)) {
-                $row = mysqli_fetch_array($result);
-                $total = $row['cost']*$count;
-                if($row['count'] >= $count and  $total <= $_SESSION['money']){
-                    $_SESSION['confirm']= "confirm";
-                    $_SESSION['total'] = $total;
-                    $_SESSION['product'] = $product;
-                    $_SESSION['count'] = $count;
-                    header("location: confirm.php");
-                }
-                else{
-                    $_SESSION['error'] = " count or tatal invalid";
-                    header("location: index.php");
-                }
-                
-            } else {
-                
-                header("location: index.php");
-            }
+if (empty($count)) {
+    array_push($errors, "count is required");
+}
+
+if (empty($product)) {
+    array_push($errors, "product is required");
+}
+if (count($errors) == 0) {
+    $query = "SELECT * FROM product WHERE name = '$product'";
+    $result = mysqli_query($conn, $query);
+    $total;
+    if (!empty($result)) {
+        $row = mysqli_fetch_array($result);
+        if (gettype($row['cost']) == "string") {
+            $total = 0;
         } else {
+            $total = $row['cost'] * $count;
+        }
+        if ($row['count'] >= $count and  $total <= $_SESSION['money']) {
+            $_SESSION['confirm'] = "confirm";
+            header("location: confirm.php?total=$total&product=$product&count=$count");
+        } else {
+            $_SESSION['error'] = " count or tatal invalid";
             header("location: index.php");
         }
+    } else {
+
+        header("location: index.php");
+    }
+} else {
+    header("location: index.php");
+}
--- confirm.php
+++ confirm.php
@@ -1,31 +1,39 @@
-
 <?php
 session_start();
 include('server.php');
 
 if (!isset($_SESSION['username'])) {
-    header('location: login.php');
+     header('location: login.php');
 }
-if (!isset($_SESSION['confirm'])){
+if (!isset($_SESSION['confirm'])) {
      header('location: index.php');
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="style.css">
      <title>Document</title>
 </head>
-<body>
-     <?php
-      echo $_SESSION['confirm'];
-      echo $_SESSION['total'];
-      echo $_SESSION['count'];
-      echo $_SESSION['product'];
 
-     ?>
+<body>
+     <form action="success.php" method="POST">
+          <div class="homeheader">
+               <p> Infomation of your cart </p>
+          </div>
+          <div class="homecontent">
+               <p> Product name : <?php $product = $_GET['product']; echo $product; ?></p>
+               <p> Count : <?php $count = $_GET['count']; echo $count; ?></p>
+               <p> Total cost : <?php $cost = $_GET['total']; echo $cost; ?> $</p>
+               
+               <input type="submit" value="confirm">
+          </div>
+     </form>
 </body>
+
 </html>
